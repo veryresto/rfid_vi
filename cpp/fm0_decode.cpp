caa664b6cf8394eeb3584b6e5a465b606bd3bc07
shingwan <veryresto@gmail.com>
2018-03-26 19:18:27 +0900

    success correct parsing

--- cpp/fm0_decode.cpp
+++ cpp/fm0_decode.cpp
@@ -12,10 +12,9 @@ int main(){
 	//int rn[17] = {};
 	//std::vector<int*> rn;
 	
-	int i, j, rx, diff = 0;
+	int i, j, diff = 0;
 	int thr = 3;
-	int sim = 0;
-	int len, cont, note;
+	int len, len1, len2, cont, note, pos1, pos2, stop1, stop2;
 	char f;
 	int m = sizeof(bits)/sizeof(int);
 	
@@ -24,43 +23,49 @@ int main(){
 	for (i = 0; i < m; i++){
 		printf("\nI-%d", i);
 		cont = 1;
-		len = 1;
+		len = 1; len1 = 1; len2 = 1;
 		f = 'n';
-		note = 9999;
+		note = 0; pos1 = 0; pos2 = 0;
+		stop1 = 0; stop2 = 0;
 		diff = 0;
-		for (j = 0; j < thr+1; j++){		
-			if(bits[i+j]==bits[i+j+1]){
-				len++;
-			}else{
-				diff++;
-				if(len<thr){
-					cont = 0;
-				
+		for (j = 0; j < thr+1; j++){
+			if(stop1 == 0){
+				// check continous bit sequence
+				if(bits[i+j]==bits[i+j+1]){
+					len1++;
+					pos1++;
+				} else {
+					stop1++;
 				}
-				note = j;
-				printf("--NOTE%d--", note);
-
-				if(j>=thr-1){
-					if(cont == 1){
-						break;
-					}
-					if(diff >= 2){
-						break;
-					}
-				}	
+			}else if(stop2 == 0){
+				// check continous bit sequence
+				if(bits[i+j]==bits[i+j+1]){
+					len2++;
+					pos2++;
+				} else {
+					stop2++;
+					note = j;
+				}
 			}
+
+			
 		}
-		if(cont==1 && j>=thr-1){
+		if(len1 >= thr){
+			i = i + pos1;
 			f = 'S';
-			note = j;
+		} else if(len2<3){
+			i = i + len1 + pos2;
+			f = 'E';
 		} else {
+			i = i + len1 + 1;
 			f = 'E';
 		}
-		
-		printf(" i-%d. len-%d cont-%d note-%d diff-%d %c", i, len, cont, note, diff, f);
-		if(note == j){
-			i = i + note;
-			//break;
-		}
+		printf(". len1-%d len2-%d note-%d pos1-%d pos2-%d %c", len1, len2, note, pos1, pos2, f);
 	}
+	printf("\n");
+	//if(bits[i+j]==bits[i+j+1]){
+		//len++;
+	//}else{
+	
+	//}
 }
